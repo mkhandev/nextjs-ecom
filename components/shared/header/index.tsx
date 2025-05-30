c88455682ed28da47c413b5618eb26e89f30ec80
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-05-30 23:32:42 +0600

    ecom settup

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,38 @@
+import { ShoppingCart, UserIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { APP_NAME } from "@/lib/constants";
+
+function Header() {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link href="/" className="flex-start">
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} logo`}
+              width={48}
+              height={48}
+              priority={true}
+            />
+
+            <span className="hidden ml-3 text-2xl font-bold lg:block">
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+        <div className="space-x-2">
+          <Button variant="ghost">
+            <Link href="/cart">
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ lib/constants/index.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Ecom";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "Ecom is a modern e-commerce platform built with Next.js and React.";
+
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
