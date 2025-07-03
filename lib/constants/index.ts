578bfbb4b6943774da5dee9bf2aa1f5183521cae
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-07-04 01:15:08 +0600

    add signout

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -10,6 +10,6 @@ export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-  email: "",
-  password: "",
+  email: "user@example.com",
+  password: "123456",
 };
