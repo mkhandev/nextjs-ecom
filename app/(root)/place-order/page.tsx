bc5087bee3f804f784ade36bca8ca67e87d05404
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-07-11 23:09:59 +0600

    order action add

--- app/(root)/place-order/page.tsx
+++ app/(root)/place-order/page.tsx
@@ -18,6 +18,7 @@ import {
 } from "@/components/ui/table";
 import Image from "next/image";
 import { formatCurrency } from "@/lib/utils";
+import PlaceOrderForm from "./place-order-form";
 
 export const metadata: Metadata = {
   title: "Place Order",
@@ -133,6 +134,7 @@ const PlaceOrderPge = async () => {
                 <div>Total</div>
                 <div>{formatCurrency(cart.totalPrice)}</div>
               </div>
+              <PlaceOrderForm />
             </CardContent>
           </Card>
         </div>
