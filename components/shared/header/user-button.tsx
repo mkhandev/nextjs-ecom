38b17f4139a56169db982bc7964d60ac16c9bfa4
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-07-13 13:56:17 +0600

    add user order

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -52,6 +52,10 @@ const UserButton = async () => {
             </div>
           </DropdownMenuLabel>
 
+          <DropdownMenuItem>
+            <Link href="/user/orders">Order History</Link>
+          </DropdownMenuItem>
+
           <DropdownMenuItem className="p-0 mb-1">
             <form action={signOutUser} className="w-full">
               <Button
